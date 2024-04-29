a49dda9bb58df53df4696a7a1efe061ca69e3556
Oh seung min <ohmin@gmail.com>
2024-04-29 20:17:33 +0900

    중복 방지 유효성 검사 추가 캐릭터 제거 기능 수정

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function addToParty(partyNum, character) {
   var party = document.getElementById("party" + partyNum);
+  
+  // 이미 등록된 캐릭터인지 확인
+  var characterBtns = party.getElementsByTagName("button");
+  for (var i = 0; i < characterBtns.length; i++) {
+      if (characterBtns[i].textContent === character) {
+          alert("이미 등록된 캐릭터입니다.");
+          return; // 이미 등록된 캐릭터면 함수 종료
+      }
+  }
+
   var characterBtn = document.createElement("button");
   characterBtn.textContent = character;
   characterBtn.onclick = function() {
-      removeFromParty(partyNum, character);
+      removeFromParty(characterBtn);
   };
   party.appendChild(characterBtn);
 }
 
-function removeFromParty(partyNum, character) {
-  var party = document.getElementById("party" + partyNum);
-  var characterBtns = party.getElementsByTagName("button");
-  for (var i = 0; i < characterBtns.length; i++) {
-      if (characterBtns[i].textContent === character) {
-          party.removeChild(characterBtns[i]);
-          break;
-      }
-  }
+function removeFromParty(characterBtn) {
+  // characterBtn의 부모 요소인 파티로부터 characterBtn 제거
+  var party = characterBtn.parentNode;
+  party.removeChild(characterBtn);
 }
